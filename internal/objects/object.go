5b3009e6830159ecf82da55ab7431fd49caa271f
naqet <naqet8@gmail.com>
2024-05-19 07:59:18 +0200

    feat: added reading objects

--- internal/objects/object.go
+++ internal/objects/object.go
@@ -5,6 +5,8 @@ import (
 	"compress/zlib"
 	"crypto/sha1"
 	"encoding/hex"
+	"errors"
+	"io"
 	"os"
 	"path"
 	"strconv"
@@ -46,10 +48,6 @@ func WriteObject(obj Object, repo *repository.Repository) (string, error) {
 		return "", err
 	}
 
-	if err != nil {
-		return "", err
-	}
-
 	hash := sha1.New()
 
 	_, err = hash.Write(buf.Bytes())
@@ -74,26 +72,109 @@ func WriteObject(obj Object, repo *repository.Repository) (string, error) {
 
 		f, err := os.Create(path.Join(r.Path, "objects", dir, file))
 
-        if err != nil {
-            return "", err
-        }
+		if err != nil {
+			return "", err
+		}
 
-        defer f.Close()
+		defer f.Close()
 
-        w := zlib.NewWriter(f)
+		w := zlib.NewWriter(f)
 
-        _, err = w.Write(buf.Bytes())
+		_, err = w.Write(buf.Bytes())
 
-        if err != nil {
-            return "", err
-        }
+		if err != nil {
+			return "", err
+		}
 
-        err = w.Close()
+		err = w.Close()
 
-        if err != nil {
-            return "", err
-        }
+		if err != nil {
+			return "", err
+		}
 	}
 
 	return sha, nil
+}
+func ReadObject(sha string, repo *repository.Repository) (Object, error) {
+	if repo == nil {
+		return nil, errors.New("Repository is not available")
+	}
+
+	if len(sha) < 3 {
+		return nil, errors.New("Invalid hash")
+	}
+
+	dir := sha[:2]
+	file := sha[2:]
+
+	f, err := os.Open(path.Join(repo.Path, "objects", dir, file))
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer f.Close()
+
+	r, err := zlib.NewReader(f)
+
+	if err != nil {
+		return nil, err
+	}
+
+	hashData := bytes.Buffer{}
+
+	_, err = io.Copy(&hashData, r)
+
+	r.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	objType, err := hashData.ReadBytes(' ')
+
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = hashData.ReadBytes('\x00')
+
+	if err != nil {
+		return nil, err
+	}
+
+    data := bytes.Buffer{}
+
+    var b byte
+
+    for {
+        b, err = hashData.ReadByte()
+        
+        if err != nil {
+            break;
+        }
+
+        err = data.WriteByte(b)
+
+        if err != nil {
+            break
+        }
+    }
+
+    if err != nil && !errors.Is(err, io.EOF) {
+        return nil, err
+    }
+
+	if len(objType) < 2 {
+		return nil, errors.New("Invalid object type")
+	}
+
+	var obj Object
+
+	switch string(objType[:len(objType)-1]) {
+	case "blob":
+		obj = NewBlob(data.Bytes())
+	}
+
+	return obj, nil
 }
