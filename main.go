8e0d023d4298b015b682cb105463b56f03990cc7
naqet <naqet8@gmail.com>
2024-07-17 12:48:53 +0200

    RESET

--- main.go
+++ main.go
@@ -1,29 +1,3 @@
 package main
 
-import (
-	"fmt"
-	"os"
-
-	"github.com/naqet/cgit/internal/commands"
-)
-
-func main() {
-	args := os.Args[1:]
-
-	if len(args) == 0 {
-		panic("You need to specify command")
-	}
-
-	cmd, ok := commands.Commands[args[0]]
-
-	if !ok {
-		fmt.Println("Invalid command")
-		return
-	}
-
-	err := cmd.Process(args[1:])
-
-	if err != nil {
-		panic(err)
-	}
-}
+func main() {}
